563492a18597ad95362ff0a0ad9782095836a23e
ksrikar1234 <49874498+ksrikar1234@users.noreply.github.com>
2023-06-15 16:14:50 +0530

    Create Cube_pan_&_rotate.cpp

---
+++ Cube_pan_&_rotate.cpp
@@ -0,0 +1,304 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+double mouse_x  = 0.0 , mouse_y = 0.0;
+float translate_mouse_x = 0.0 , translate_mouse_y = 0.0 ;
+
+
+GLfloat cubeVertices[] = {
+    // Positions         // Colors
+    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+    0.5f, 0.5f, -0.5f,1.0f, 0.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 1.0f,
+    -0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 0.0f,
+    0.5f, -0.5f, 0.5f, 0.0f, 1.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+    0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+    -0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+
+     0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+     0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+     0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 1.0f,
+     0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+     0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f,
+
+     -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+     0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+     0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+
+    -0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
+     0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+     0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+     0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+    -0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 0.0f,
+    -0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f
+
+};
+
+const char* vertexShaderSource = R"(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aColor;
+
+    out vec3 ourColor;
+
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
+
+    void main()
+    {
+        gl_Position = projection * view * model * vec4(aPos, 1.0);
+        ourColor = aColor;
+    }
+)";
+
+const char* fragmentShaderSource = R"(
+    #version 330 core
+    in vec3 ourColor;
+    out vec4 FragColor;
+
+    void main()
+    {
+        FragColor = vec4(ourColor, 1.0);
+    }
+)";
+
+void mousePositionCallback(GLFWwindow* window, double xpos, double ypos)
+{
+    // Print mouse position to console
+    std::cout << "Mouse position: " << xpos << ", " << ypos << std::endl;
+    
+    
+    int rightButtonState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
+    int leftButtonState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+    // Check if the right mouse button is pressed
+     // {
+    //    std::cout << "Right mouse button clicked at position: (" << x << ", " << y << ")" << std::endl; }
+    
+    
+    if (rightButtonState == GLFW_PRESS) 
+       {
+        std::cout << "Right mouse button clicked! Rotation mode active " << '\n';
+        mouse_x = xpos;
+        mouse_y = ypos;
+       }
+     
+     if (leftButtonState == GLFW_PRESS) 
+       {
+        std::cout << "Right mouse button clicked! translation mode active " << '\n';
+        translate_mouse_x = xpos;
+        translate_mouse_y = ypos;
+       }
+         
+       
+       
+       
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+
+
+
+
+
+int main()
+{
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Set OpenGL version and profile to 3.3 core
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create a GLFW window
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Cube Rotation", nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    
+    glfwSetCursorPosCallback(window, mousePositionCallback);
+ //   glfwSetCursorPosCallback(window, initialPositionCallback);
+
+    // Initialize GLEW
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Create vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    // Check for vertex shader compile errors
+    GLint success;
+    GLchar infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        std::cerr << "Vertex shader compilation failed:\n" << infoLog << std::endl;
+    }
+
+    // Create fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    // Check for fragment shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        std::cerr << "Fragment shader compilation failed:\n" << infoLog << std::endl;
+    }
+
+    // Create shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Check for shader program link errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cerr << "Shader program linking failed:\n" << infoLog << std::endl;
+    }
+
+    // Delete shaders as they're linked into the program now and no longer needed
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Create vertex buffer object (VBO) and vertex array object (VAO)
+    GLuint VBO, VAO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+
+    // Bind VAO first, then bind and set vertex buffer(s), and then configure vertex attribute(s)
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Color attribute
+    
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    // Unbind VBO and VAO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Enable depth testing
+    glEnable(GL_DEPTH_TEST);
+
+    // Render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Input handling
+        processInput(window);
+
+        // Clear the screen
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Activate shader program
+        glUseProgram(shaderProgram);
+
+        // Create transformations
+        
+        
+        float time = glfwGetTime();
+        float mouse_input = float((mouse_x/WIDTH + mouse_y/HEIGHT)*3.14);
+        
+        float set_axis_x =  mouse_x/WIDTH , set_axis_y = mouse_y/HEIGHT;
+        
+        
+        glm::mat4 model = glm::rotate(glm::mat4(1.0f), mouse_input, glm::vec3(set_axis_x, set_axis_y, 0.0f));
+        
+        //translate_mouse_x = 0.0f;
+        
+        glm::mat4 view = glm::translate(glm::mat4(0.1f), glm::vec3(translate_mouse_x/WIDTH, translate_mouse_y/HEIGHT, -3.0f));
+        
+        
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+
+        // Pass transformation matrices to the shader
+        
+        
+        GLint modelLoc = glGetUniformLocation(shaderProgram, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        GLint viewLoc = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        GLint projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        // Draw cube
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Clean up resources
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+    glfwTerminate();
+    return 0;
+}
